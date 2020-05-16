b430acca8d6b28f8fb3bd2aa668e54e30ab9bb14
dz-s <shulhindvst@gmail.com>
2020-05-16 20:38:13 +0200

    upd

---
+++ algorithms/Javascript/minimum-window-substring.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
+
+ //TODO
+var minWindow = function (s, t) {
+    let minLen = t.length
+    let sLen = s.length
+    let tArr = t.split("")
+    let j = 0
+    // for (let i = 0; i < sLen; i++) {
+    //     j = 0
+    //     if(s.charAt(i) === tArr[j]){
+    //         for
+    //     }
+    // }
+
+};
+
+console.log(minWindow("ADOBECODEBANC", "ABC"))
